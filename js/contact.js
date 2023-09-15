fbfc4a0656975f329bfba8926e6fba5526746caf
Prakash kumar nayak <prakashkumarn413@gmail.com>
2023-09-15 13:19:30 +0530

    Animations added

--- js/contact.js
+++ js/contact.js
@@ -4,15 +4,14 @@ function sendMail() {
     })();
 
     var params = {
-        from_name: document.querySelector("#parentname").value,
-        st_name: document.querySelector("#studentname").value,
+        from_name: document.querySelector("#studentname").value,
         email: document.querySelector("#parentemail").value,
         phone: document.querySelector("#phone").value,
-        class: document.querySelector("#course").value,
+        message: document.querySelector("#message").value,
     };
 
     var serviceID = "service_4jbxtbv"; // Email Service ID
-    var templateID = "template_hel0vbt"; // Email Template ID
+    var templateID = "template_g52vhrc"; // Email Template ID
 
     emailjs.send(serviceID, templateID, params)
         .then(res => {
